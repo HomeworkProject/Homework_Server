29e7366ac3be1fa055128089312ba19357d73e2b
Magnus Leßmann (aka. mark332) <github@m-lessmann.de>
2016-06-22 22:14:45 +0200

    Enabled multithread compatibility for the main Server

---
+++ src/main/java/de/mlessmann/hwserver/HWServerRunnable.java
@@ -0,0 +1,36 @@
+package de.mlessmann.hwserver;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * Created by Life4YourGames on 09.06.16.
+ */
+public class HWServerRunnable implements Runnable {
+
+    private HWServer server;
+    public Exception lastError;
+
+    public HWServerRunnable(HWServer myServer) {
+
+        server = myServer;
+
+    }
+
+    public void run() {
+
+        try {
+
+            server.preInitialize();
+
+            server.start();
+
+        } catch (IOException ex) {
+
+            lastError = ex;
+
+        }
+
+    }
+
+}
