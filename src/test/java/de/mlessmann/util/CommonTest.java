4dbd6fb1daea556ced6b256f2689280ad3826307
Magnus Leßmann (aka. mark332) <github@m-lessmann.de>
2016-07-07 20:52:22 +0200

    Moved CommonTest
    ... to test module

---
+++ src/test/java/de/mlessmann/util/CommonTest.java
@@ -0,0 +1,40 @@
+package de.mlessmann.util;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by Life4YourGames on 05.07.16.
+ */
+public class CommonTest {
+
+    @Test
+    public void negateInt() throws Exception {
+
+        Assert.assertEquals(-2000, Common.negateInt(2000));
+
+    }
+
+    @Test
+    public void getFirstVersion() throws Exception {
+
+        Assert.assertEquals("4.5.0.102.2", Common.getFirstVersion("iaehfubezgwhwieivwi v4.5.0.102.2 fjeiwj"));
+
+    }
+
+    @Test
+    public void compareVersions() throws Exception {
+
+        Assert.assertEquals(0, Common.compareVersions("2.7.8", "2.7.8"));
+
+        Assert.assertEquals(0, Common.compareVersions("2.7.8.0", "2.7.8"));
+
+        Assert.assertEquals(0, Common.compareVersions("2.7.8", "2.7.8.0"));
+
+        Assert.assertEquals(-1, Common.compareVersions("2.7.7", "2.7.8"));
+
+        Assert.assertEquals(1, Common.compareVersions("2.7.7", "2.7.6"));
+
+    }
+
+}
